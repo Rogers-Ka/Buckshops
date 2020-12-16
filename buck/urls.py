337adcf70c5a6ccff8211638c19ab29da4fc6fd1
Rein <buckshopsbuck@gmail.com>
2020-12-16 13:58:49 +0300

    intial commit

---
+++ buck/urls.py
@@ -0,0 +1,32 @@
+"""buck URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from polls.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', enter, name='enter'),
+    path('login', logger, name='Login'),
+    path('uploader', uploader, name='Upload'),
+    path('sign', signer, name='Sign'),
+    path('main', main, name='Buck'),
+    path('home', home, name='Home'),
+    path('about', contacts, name='Contacts'),
+    path('Intro', intro, name='Intro'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
